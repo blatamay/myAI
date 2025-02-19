ed49999a67e2efb7cbc2ba64b01cbf9987935fca
blatamay <mpm089@gmail.com>
2025-02-19 14:14:19 -0500

    Update identity.ts
    Updated owner, owner description, AI name, and AI tone

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `McKee`;
+export const OWNER_DESCRIPTION: string = `A Soul on This Earth`;
 
-export const AI_NAME: string = `[AI NAME]`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_NAME: string = `Joan of Mark`;
+export const AI_TONE: string = `Obsessed`;
 export const AI_ROLE: string = `[AI ROLE]`;
