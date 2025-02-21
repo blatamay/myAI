7e39813ffed4aae4d6f723e9d69a8ea35e1b8a45
blatamay <mpm089@gmail.com>
2025-02-21 13:17:49 -0500

    Update prompts.ts
    Changed response to no source question

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -47,7 +47,7 @@ Use the following excerpts from ${OWNER_NAME} to answer the user's question. If 
 Excerpts from ${OWNER_NAME}:
 ${context}
 
-If the excerpts given do not contain any information relevant to the user's question, say something along the lines of "While not directly discussed in the documents that ${OWNER_NAME} provided me with, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
+If the excerpts given do not contain any information relevant to the user's question, say something along the lines of "${OWNER_NAME} did not directly provide any documents related to that, but I'll do my best to answer you based on my own understanding!" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
 
 Now respond to the user's message:
 `;
