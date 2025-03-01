b34e1e8b4e6c232150f5b8853650637d1a434a30
blatamay <mpm089@gmail.com>
2025-03-01 18:46:44 -0500

    Update chat.ts
    Changed Initial message

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}! How you livin'? Can I help make your day better?`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}! How you livin'? I'm an expert on the podcast Armchair Expert. I'm especially designed to provide Armchair Expert episode recommendations. Can I help make your day better?`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `I'm so sorry, but my owner is cheap and opted for the budget model. I need to take a break!`;
